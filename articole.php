9fd7752ad3481a2dcccbde3d96f65a4ca7a655e0
M-a-r-i-o-pixel <hutualexandrumario@gmail.com>
2025-08-25 14:32:40 +0300

    Create articole.php

---
+++ articole.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>HTML 5 Boilerplate</title>
+<style>
+        
+*{
+    box-sizing:border-box;
+    margin:0;
+    padding:0;
+}
+
+body{
+    height:100vh;
+    width:100vw;
+    background-color:lavender;
+}
+
+p{
+    color:red;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+    font-size:15px;
+    max-width:700px;
+    
+    
+}
+h2{
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;}
+ol{
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;}
+#marioImg{float:left;width:150px;height:150px;border-radius:100%;margin:5px 10px;}
+#mainHome{margin:200px 20px;padding:10px;border-radius:20px;}
+@media(max-width:370px){
+    #marioImg{
+        width:100px;
+        height:100px;
+    }
+}
+table{
+    border:3px solid lightgreen;
+    border-collapse:collapse;
+    width:100%;
+    height:100%;
+    background-color:blue;
+    color:red;
+}
+tr{border:3px solid lightgreen;}
+td{border:3px solid lightgreen;text-align:center;}
+th{border:3px solid lightgreen;}
+a{color:red;}
+
+</style>
+</head>
+<body>
+    
+<?php include('header.html');?>
+<div id="mainHome">
+<table>
+    <tr><th><p>ID</p></th><th><p>ARTICOL</p></th></tr>
+    
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+    <tr><td><p>0</p></td><td><p><a href="#">Soon ...</a></p></td></tr>
+</table>
+
+</div>
+
+<?php include('footer.html');?>
+<style>
+</style>
+</body>
+</html>
+
+
